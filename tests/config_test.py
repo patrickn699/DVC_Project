60e2aaab3f671026f4fb39253607c1d787335e5a
patrickn699 <52091202+patrickn699@users.noreply.github.com>
2021-03-19 17:42:09 +0530

    pytest example

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,6 +1,17 @@
+import pytest
 
+class OutOfRange(Exception):
+    def __init__(self,message = 'the value is out of range'):
+
+        self.message = message
+        super().__init__(self.message)
 
 def test_generic():
-    a = 8
-    b = 8
-    assert a== b
+    a = 5
+    with pytest.raises(OutOfRange):
+        if a not in range(10,20):
+            raise OutOfRange
+
+   
+    
+    #assert a== b
