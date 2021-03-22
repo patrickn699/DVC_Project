996d06b7b925c2ee18d99532feef661b2cdd9aba
patrickn699 <52091202+patrickn699@users.noreply.github.com>
2021-03-22 22:48:52 +0530

    added a comment in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 from prediction_service import predictions
 
-
+# This file is for an api call!!
 webapp_root = "webapp"
 
 static_dir = os.path.join(webapp_root, "static")
